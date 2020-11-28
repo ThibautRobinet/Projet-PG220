c1ac40cb4d7530341335f688e5085e400fb4f7a5
Scialom Zachary <zscialom@enseirb-matmeca.fr>
2020-11-28 18:05:35 +0100

    Ameliorations graphiques

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,4 +1,15 @@
 import information.*;
+
+import javax.security.auth.login.Configuration;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import game.GameHandle;
 import game.StandardGameHandle;
 import information.UserInterface;
@@ -8,6 +19,29 @@ public class Test {
 	public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
         fenetre.setVisible(true);
+        Fenetre options = new Fenetre();
+        options.setTitle("Options de configuration");
+        options.setSize(300, 700);
+
+        JPanel container = new JPanel();
+        String[] tab = {"humain", "ia"};
+        JComboBox combo = new JComboBox(tab);
+        JLabel label = new JLabel("Choix utilisateur");
+
+        container.setBackground(Color.white);
+        container.setLayout(new BorderLayout());
+        combo.setPreferredSize(new Dimension(100, 30));
+
+        JPanel top = new JPanel();
+        top.add(label);
+        top.add(combo);
+        container.add(top, BorderLayout.NORTH);
+        options.setContentPane(container);     
+        options.setVisible(true);
+        
+
+        
+
      }
 
 
--- src/main/java/information/Fenetre.java
+++ src/main/java/information/Fenetre.java
@@ -1,5 +1,10 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 //Utilisation API --> SWING
 
 import java.io.BufferedReader;
@@ -10,12 +15,15 @@ import java.io.InputStreamReader;
 
 
 public class Fenetre extends JFrame {
+    private JPanel container = new JPanel();
+    private JComboBox combo = new JComboBox();
+    private JLabel label = new JLabel();
 
     public Fenetre() {
-       setTitle("Fenetre TEST");
-       setSize(300, 200);
-       setLocationRelativeTo(null);
-       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       this.setTitle("Fenetre de jeu");
+       this.setSize(1000, 700);
+       this.setLocationRelativeTo(null);
+       this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 
